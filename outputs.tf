afcb7a30d2c867644ed1ab49d514cee56f5d2866
Daniel Allen <dan@aeternagroup.net>
2026-07-16 15:01:49 +0100

    Update to schema version v6.13.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "actions_organization_variable_repositories_id" {
   description = "Map of id values across all actions_organization_variable_repositories, keyed the same as var.actions_organization_variable_repositories"
-  value       = { for k, v in github_actions_organization_variable_repository.actions_organization_variable_repositories : k => v.id }
+  value       = { for k, v in github_actions_organization_variable_repository.actions_organization_variable_repositories : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "actions_organization_variable_repositories_repository_id" {
   description = "Map of repository_id values across all actions_organization_variable_repositories, keyed the same as var.actions_organization_variable_repositories"
-  value       = { for k, v in github_actions_organization_variable_repository.actions_organization_variable_repositories : k => v.repository_id }
+  value       = { for k, v in github_actions_organization_variable_repository.actions_organization_variable_repositories : k => v.repository_id if v.repository_id != null }
 }
 output "actions_organization_variable_repositories_variable_name" {
   description = "Map of variable_name values across all actions_organization_variable_repositories, keyed the same as var.actions_organization_variable_repositories"
-  value       = { for k, v in github_actions_organization_variable_repository.actions_organization_variable_repositories : k => v.variable_name }
+  value       = { for k, v in github_actions_organization_variable_repository.actions_organization_variable_repositories : k => v.variable_name if v.variable_name != null && length(v.variable_name) > 0 }
 }
 
